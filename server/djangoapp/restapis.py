cf73a311f95de251911238b15ca6f929ca85789b
Shams Sulaiman Fida <104012226+ProDev06@users.noreply.github.com>
2026-06-25 15:09:11 +0600

    Fix exception handling in API request functions

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -22,11 +22,12 @@ sentiment_analyzer_url = os.getenv(
 # def post_review(data_dict):
 # Add code for posting review
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -35,9 +36,8 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
         # If any error occurs
-        print("Network exception occurred")
+    print("Network exception occurred")
 
 
 def analyze_review_sentiments(text):
@@ -57,5 +57,4 @@ def post_review(data_dict):
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    print("Network exception occurred")
